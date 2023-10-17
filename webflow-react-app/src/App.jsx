2d883a8640eab5034369dcb8870069fff8751fa3
Muh-Wale <muhammadolawale27@gmail.com>
2023-10-17 13:49:20 +0100

    Commit

--- webflow-react-app/src/App.jsx
+++ webflow-react-app/src/App.jsx
@@ -1,6 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import { BrowserRouter, Routes, Poute } from 'react-router-dom/cjs/react-router-dom.min'
 import './App.css'
+import Home from './Home/Home'
+import HomeTwo from './Home.V2/HomeTwo'
+import About from './About/About'
+import Blog from './Blog/Blog'
+import Services from '.Services/Services'
+import Projects from './Projects/Projects'
+import Contact from './Contact/Contact'
+
 
 function App() {
 
@@ -8,9 +16,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route>
-            
-          </Route>
+          <Route index element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/hometwo" element={<HomeTwo/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/blog" element={<Blog/>}/>
         </Routes>
       </BrowserRouter>
     </div>
