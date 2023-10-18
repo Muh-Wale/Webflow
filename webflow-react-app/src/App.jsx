c7f2170a7da9793dbd48d471b8f333a31efd8aa6
Muh-Wale <muhammadolawale27@gmail.com>
2023-10-18 21:13:34 +0100

    fourth commit

--- webflow-react-app/src/App.jsx
+++ webflow-react-app/src/App.jsx
@@ -5,9 +5,19 @@ import Home from './Home/Home'
 import HomeTwo from './Home.V2/HomeTwo'
 import About from './About/About'
 import Blog from './Blog/Blog'
-import Services from '.Services/Services'
+import Services from './Services/Services'
 import Projects from './Projects/Projects'
 import Contact from './Contact/Contact'
+import BlogSingle from './Blog Single/BlogSingle'
+import ProjectSingle from './Project Single/ProjectSingle'
+import ServiceSingle from './Service Single/ServiceSingle'
+import Shop from './Shop/Shop'
+import ShopCategories from './Shop Categories/ShopCategories'
+import ShopSingle from './Shop Single/ShopSingle'
+import Checkout from './Checkout/Checkout'
+import CheckoutPaypal from './Checkout Paypal/CheckoutPaypal'
+import OrderConfirmation from './Order Confirmation/OrderConfirmation'
+import Error from './Error/Error'
 
 
 function App() {
@@ -21,6 +31,19 @@ function App() {
           <Route path="/hometwo" element={<HomeTwo/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/blog" element={<Blog/>}/>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/blogsingle" element={<BlogSingle/>}/>
+          <Route path="/ProjectSingle" element={<ProjectSingle/>}/>
+          <Route path="/servicesingle" element={<ServiceSingle/>}/>
+          <Route path="/shop" element={<Shop/>}/>
+          <Route path="/shopcategories" element={<ShopCategories/>}/>
+          <Route path="/shopsingle" element={<ShopSingle/>}/>
+          <Route path="/checkout" element={<Checkout/>}/>
+          <Route path="/checkoutpaypal" element={<CheckoutPaypal/>}/>
+          <Route path="/orderconfirmation" element={<OrderConfirmation/>}/>
+          <Route path="*" element={<Error/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- webflow-react-app/src/Home.V2/pages/HtService.jsx
+++ webflow-react-app/src/Home.V2/pages/HtService.jsx
@@ -4,9 +4,9 @@ import { BsArrowRight } from 'react-icons/bs'
 const HtService = () => {
     return (
         <div className='max-w-[1640px] mx-auto'>
-            <div className=' bg-[#E5E5E5] max-w-[1640px] mx-auto px-5 md:px-14 lg:px-24'>
+            <div className=' md:bg-[#E5E5E5] bg-none max-w-[1640px] mx-auto px-5 md:px-14 lg:px-24'>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-5 pt-10 md:pt-0 pb-12 max-w-[1300px] mx-auto md:relative static z-10 md:top-[-70px] top-0'>
-                    <div>
+                    <div className=' hover:shadow-lg shadow-md md:shadow-none'>
                         <div className='p-3 md:p-6 lg:p-10 bg-white  max-w-[400px] rounded mx-auto md:mx-0'>
                             <div className=''>
                                 <img src="/src/Pic/18.png" alt=""/>
@@ -18,7 +18,7 @@ const HtService = () => {
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div className=' hover:shadow-lg shadow-md md:shadow-none'>
                         <div className='p-3 md:p-6 lg:p-10 bg-white max-w-[400px] md:max-w-[400px] rounded mx-auto md:mx-0'>
                             <div className=''>
                                 <img src="/src/Pic/19.png" alt=""/>
@@ -30,7 +30,7 @@ const HtService = () => {
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div className=' hover:shadow-lg shadow-md md:shadow-none'>
                         <div className='p-3 md:p-6 lg:p-10 bg-white max-w-[400px] md:max-w-[400px] rounded mx-auto md:mx-0'>
                             <div className=''>
                                 <img src="/src/Pic/20.png" alt=""/>
@@ -45,11 +45,11 @@ const HtService = () => {
                     
                 </div>
             </div>
-            <div className='max-w-[1640px] mx-auto px-5 md:px-14 lg:px-24 my-10'>
-                <div className='max-w-[1300px] mx-auto py-5 flex justify-between gap-5 border-[#E5E5E5] border-b-4'>
-                    <div className='flex justify-center w-full font-semibold text-xl'>Service One</div>
-                    <div className='flex justify-center w-full font-semibold text-xl'>Service Two</div>
-                    <div className='flex justify-center w-full font-semibold text-xl'>Service Three</div>
+            <div className='max-w-[1640px] mx-auto px-3 md:px-14 lg:px-24 my-10'>
+                <div className='max-w-[1300px] mx-auto py-5 flex justify-between gap-0 md:gap-5 border-[#E5E5E5] border-b-4'>
+                    <div className='flex justify-center w-full font-semibold text-xl hover:text-[#FC5623]'>Service One</div>
+                    <div className='flex justify-center w-full font-semibold text-xl hover:text-[#FC5623]'>Service Two</div>
+                    <div className='flex justify-center w-full font-semibold text-xl hover:text-[#FC5623]'>Service Three</div>
                 </div>
             </div>
         </div>
